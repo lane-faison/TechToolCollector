9064d9e386f7da32fbbc3ba13955665841c1f963
Lane Faison <lane.faison@icloud.com>
2017-04-29 14:45:12 -0600

    Updates

---
+++ TechToolCollector/TechnologyViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TechnologyViewController.swift
+//  TechToolCollector
+//
+//  Created by Lane Faison on 4/29/17.
+//  Copyright © 2017 Lane Faison. All rights reserved.
+//
+
+import UIKit
+
+class TechnologyViewController: UIViewController {
+
+    @IBOutlet weak var techImageView: UIImageView!
+    
+    @IBOutlet weak var titleTextView: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
