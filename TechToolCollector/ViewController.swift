dceff77e1d1ca6f30e7f27f94b17dd906100805a
Lane Faison <lane.faison@icloud.com>
2017-04-29 23:12:26 -0600

    Photo add works

--- TechToolCollector/ViewController.swift
+++ TechToolCollector/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var techs : [Technology] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do {
+            techs = try context.fetch(Technology.fetchRequest())
+            tableView.reloadData() // ensures table is reloaded after adding a new picture
+        } catch {
+            
+        }
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return techs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let tech = techs[indexPath.row]
+        cell.textLabel?.text = tech.title
+        cell.imageView?.image = UIImage(data: tech.image as! Data)
+        return cell
+    }
 }
 
